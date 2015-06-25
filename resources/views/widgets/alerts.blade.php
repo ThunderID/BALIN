fe9969028f7edb08f1c4d492f7281fd82cc053f1
budi_p <budi-purnomo@outlook.com>
2015-06-25 12:06:30 +0700

    fix #45

---
+++ resources/views/widgets/alerts.blade.php
@@ -0,0 +1,4 @@
+<!-- cek apa punya pesan error dan pesan dengan tipe pesan apa yg ditampilkan -->
+<div class="alert alert-danger">
+	<strong>Success!</strong> Indicates a successful or positive action.
+</div>
