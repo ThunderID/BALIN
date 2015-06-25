b2eb08f59f4e600fb490dd49905ebe16f7304e8b
budi_p <budi-purnomo@outlook.com>
2015-06-25 12:06:52 +0700

    fix #44

---
+++ resources/views/widgets/login.blade.php
@@ -0,0 +1,16 @@
+<form role="form">
+    <div class="form-group">
+        <label for="email">Email address:</label>
+        <input type="email" class="form-control" id="email" tabindex="1" required>
+    </div>
+    <div class="form-group">
+	    <label for="pwd">Password:</label>
+	    <input type="password" class="form-control" id="pwd" tabindex="1" required>
+	</div>
+	<div class="checkbox">
+	    <label><input type="checkbox" tabindex="1"> Remember me</label>
+	</div>
+	<div class="form-group">
+	    <button type="submit" class="btn btn-default" tabindex="1">Submit</button>
+	</div>
+</form>
