015cc92834f7a47e58accfd322457e50e2d3f958
budi_p <budi-purnomo@outlook.com>
2015-06-25 11:06:38 +0700

    fix #54

---
+++ resources/views/widgets/slider.blade.php
@@ -0,0 +1,47 @@
+<!-- Indicators -->
+<ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+</ol>
+
+<!-- Wrapper for Slides -->
+<div class="carousel-inner">
+    <div class="item active">
+        <!-- Set the first background image using inline CSS below. -->
+        <div class="fill" style="background-image:url('Balin/Image/3.jpg');"></div>
+        <div class="carousel-caption">
+            <h2><a href=#>Caption 1</a></h2>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        </div>
+    </div>
+    <div class="item">
+        <!-- Set the second background image using inline CSS below. -->
+        <div class="fill" style="background-image:url('Balin/Image/1.jpg');"></div>
+        <div class="carousel-caption">
+            <h2><a href=#>Caption 2</a></h2>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        </div>
+    </div>
+    <div class="item">
+        <!-- Set the third background image using inline CSS below. -->
+        <div class="fill" style="background-image:url('Balin/Image/2.jpg');"></div>
+        <div class="carousel-caption">
+            <h2><a href=#>Caption 3</a></h2>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        </div>
+    </div>
+</div>
+
+<!-- Controls -->
+<span class="left carousel-control">
+    <a class="icon-next" href="#myCarousel" data-slide="prev">
+        <i class="fa fa-chevron-circle-left"></i>
+    </a>
+</span>
+
+<span class="right carousel-control">
+    <a class="icon-next" href="#myCarousel" data-slide="next">
+        <i class="fa fa-chevron-circle-right"></i>
+    </a>
+</span>
