56f54bba9962fc06d9d375040c53068661008c10
budi_p <budi-purnomo@outlook.com>
2015-08-12 16:16:14 +0700

    fix issue #90

---
+++ resources/views/widgets/forms/formUploadImage1.blade.php
@@ -0,0 +1,25 @@
+<form role="form" style="padding-right:inherit;padding-left:inherit;">    
+    <div class="row">
+        <div class="col-sm-3 col-xs-3">                             
+            <div class="row">
+                <div class="col-md-12">
+                    <img style ="width:100%;" src="https://placeholdit.imgix.net/~text?txtsize=30&txt=320%C3%97150&w=320&h=320" alt="Profile" clas="img-responsive">
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-9 col-xs-9"> 
+            <div class="row">
+                </br>
+                <p>Click this button bellow to browse image for your profile</p>
+                <p>* Max image size : 1,5 MB</p>
+            </div>
+            <div class="row">
+                <button type="button" class="btn btn-md btn-default">browse</button>
+            </div>
+        </div>
+    </div>
+    </br>  
+	<div class="form-group">
+	    <button type="submit" class="btn btn-md btn-info">Save</button>
+	</div>
+</form>	
