bbbde6de434ce45fe541e6a69bc1151e00a981c7
budi_p <budi-purnomo@outlook.com>
2015-08-13 14:11:45 +0700

    error page

---
+++ resources/views/pages/error.blade.php
@@ -0,0 +1,5 @@
+@extends('template.frontend.layout')
+
+@section('content')
+    @include('widgets.pageElements.error', array('errorCode' => '404'))                                                                                                              
+@stop
