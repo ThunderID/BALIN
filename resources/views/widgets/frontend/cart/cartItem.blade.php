fdecf1e1f8077415b9fad20f54962d6a6067715f
budi_p <budi-purnomo@outlook.com>
2015-08-21 17:09:35 +0700

    improvement #87

---
+++ resources/views/widgets/frontend/cart/cartItem.blade.php
@@ -0,0 +1,63 @@
+<div class="row">
+    <div class="col-xs-12" style="margin:5px;">
+        <div class="row">
+            <div class="col-xs-3">
+                <a href="#">
+                    <img class="image-responsive" style="height:80px;width:60px;"  src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" >
+                </a>
+            </div>
+            <div class="col-xs-8">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h4>{{$labelName}}</h4>
+                    </div>
+                </div>
+                <div class="row" style="margin-top:0px;">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <p>Qty</p>
+                            </div>
+                            <div class="col-xs-1">
+                                <p>:</p>
+                            </div>
+                            <div class="col-xs-6" style="padding-left: 2px;">
+                                <p>{{$labelQty}}</p>
+                            </div>                                                                                                                
+                        </div>
+                    </div>
+                </div>
+                <div class="row" style="margin-top:0px;">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <p>Price/Item</p>
+                            </div>
+                            <div class="col-xs-1">
+                                <p>:</p>
+                            </div>
+                            <div class="col-xs-6" style="padding-left: 2px;">
+                                <p>{{$labelPrice}}</p>
+                            </div>                                                                                                                
+                        </div>                                                    
+                    </div>
+                </div>                                                    
+                <div class="row" style="margin-top:0px;">
+                    <div class="col-xs-12"> 
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <p>Total</p>
+                            </div>
+                            <div class="col-xs-1">
+                                <p>:</p>
+                            </div>
+                            <div class="col-xs-6" style="padding-left: 2px;">
+                                <p>{{$labelTotal}}</p>
+                            </div>                                                                                                                
+                        </div>                                             
+                    </div>                                                
+                </div>                                                                                         
+            </div> 
+        </div>
+    </div>                                                                                                                       
+</div>
---
+++ resources/views/widgets/frontend/topMenu/cartDropdown.blade.php
@@ -0,0 +1,35 @@
+<ul class="dropdown-menu dropdown-menu-right chart-dropdown" aria-labelledby="dLabel">
+    <li class="chart-dropdown-item-grid">
+        @include('widgets.frontend.cart.cartItem', array(
+            'labelName'             => 'Batik Andong Cap Jagung',
+            'labelQty'              => '1',
+            'labelPrice'            => 'RP 1.200.000',
+            'labelTotal'            => 'Rp 1.200.000'
+        ))
+    </li>
+    <li class="chart-lowLine">
+        @include('widgets.frontend.cart.cartItem', array(
+            'labelName'             => 'Batik Polong Cap Singkong',
+            'labelQty'              => '2',
+            'labelPrice'            => 'RP 600.000',
+            'labelTotal'            => 'Rp 1.200.000'
+        ))
+    </li>                        
+    <li class="chart-dropdown-subtotal chart-lowLine">
+        <div class="row">
+            <div class="col-sm-12" style="margin:5px;">
+                <p class="text-center">SUBTOTAL <Span style="font-weight:400;margin-left:25px;"> RP 11.234.123</span></p>
+            </div>
+        </div>
+    </li>  
+    <li>
+        <div class="row">
+            <div class="col-xs-12 text-center" style=" ">
+                <a href="{{ URL::route('frontend.cart.index') }}" class="btn btn-sm btn-info btn-text-white">Open Cart</a>                                   
+                <button type="button" class="btn btn-sm btn-success">
+                    Checkout
+                </button>   
+            </div>
+        </div>
+    </li>                                                                    
+</ul>
