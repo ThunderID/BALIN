ef2227a6f266f79015ace05192790ef41dcd11fd
chelsymooy <chelsymooy1108@gmail.com>
2015-10-07 11:07:04 +0700

    repair courier table

--- database/migrations/2015_06_19_034756_create_courier_table.php
+++ database/migrations/2015_06_19_034756_create_courier_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAttributeTable extends Migration
+class CreateCourierTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateAttributeTable extends Migration
      */
     public function up()
     {
-        Schema::create('attributes', function (Blueprint $table) {
+        Schema::create('couriers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 255);
             $table->string('logo_url', 255);
