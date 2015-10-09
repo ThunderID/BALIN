a8a5c9a96e77f9cce8d4a3cfca4ad9f223b538ed
uchuy77 <agil.mahendra@gmail.com>
2015-10-09 14:06:35 +0700

    modified supplier use @inject

--- resources/views/pages/backend/supplier/create.blade.php
+++ resources/views/pages/backend/supplier/create.blade.php
@@ -1,63 +1,52 @@
+@inject('data', 'App\Models\Supplier')
+{!! $data = $data::find($id) !!}
+
 @extends('template.backend.layout') 
 
 @section('content')
-    {!! Form::open(array('route' => 'backend.supplier.save')) !!}
-        {!!
-            Form::input(
-                'hidden', 
-                'id', 
-                $data['id'],  
-                [
+    {!! Form::open(array('route' => 'backend.supplier.store')) !!}
+        {!! Form::input('hidden', 'id', $data['id'],  [
                     'class' => 'mod_id'
-                ]
-            ) 
+            ]) 
         !!}    
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="name" class="text-capitalize">Nama</label>
-                    {!! 
-                        Form::text(
-                            'name',
-                            $data['name'], 
-                            [
+                    {!! Form::text('name',$data['name'], [
                                 'class'         => 'form-control', 
                                 'tabindex'      => '1', 
                                 'required'      => 'required', 
                                 'placeholder'   => 'Masukkan nama supplier'
-                            ] 
-                        ) 
+                        ]) 
                     !!}
-                </div>              
+                </div>
+            </div>
+            <div class="col-md-6">
                 <div class="form-group">
                     <label for="phone" class="text-capitalize">Nomor Telepon</label>
-                    {!! 
-                        Form::text(
-                            'phone', 
-                            $data['phone'], 
-                            [
+                    {!! Form::text('phone', $data['phone'], [
                                 'class'         => 'form-control', 
                                 'required'      => 'required', 
                                 'tabindex'      => '2',
                                 'placeholder'   => 'Masukkan nomor telepon'
-                            ] 
-                        ) 
+                        ]) 
                     !!}
                 </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
                 <div class="form-group">
                     <label for="address" class="text-capitalize">Alamat</label>
-                    {!!
-                        Form::textarea(
-                            'address',  
-                            $data['address'], 
-                            [
+                    {!! Form::textarea('address',  $data['address'], [
                                 'class'         => 'form-control',
                                 'rows'          => '3', 
                                 'required'      => 'required',
                                 'tabindex'      => '3',
                                 'style'         => 'resize:none;',
                                 'placeholder'   => 'Masukkan alamat lengkap'
-                            ]) 
+                        ]) 
                     !!}                                          
                 </div>                 
                 </br>
