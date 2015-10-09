ebfbf7fe176b7a162697692866135f838512e7c8
uchuy77 <agil.mahendra@gmail.com>
2015-10-09 14:06:21 +0700

    modified nav_sidebar

--- resources/views/widgets/backend/pageElements/nav.blade.php
+++ resources/views/widgets/backend/pageElements/nav.blade.php
@@ -8,68 +8,65 @@
                 <a href="#"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboard</span></a>
             </li>
             <li class="@if($nav_active=='administrative') active @endif }}">
-                <a href="#"><i class="fa fa-unlock-alt"></i> <span class="nav-label">Administrative</span></a>
+                <a href="#"><i class="fa fa-archive"></i> <span class="nav-label">Data</span></a>
                 <ul class="nav nav-second-level">
-                    <li><a href="#">Admin</a></li>
-                    <li><a href="#">Data User</a></li>
-                    <li><a href="#">Ganti Password User</a></li>
-                    <li><a href="#">Blokir User</a></li>
-                    <li><a href="#">Log User</a></li>
-                    <li><a href="#">Log Aplikasi</a></li>
+                    <li>
+                        <a href="#"><i class="fa fa-glass"></i> <span class="nav-label">Product</span></a>
+                        <ul class="nav nav-third-level">
+                            <li><a href="#">Stock</a></li>
+                            <li><a href="{{ route('backend.price.index') }}">Price</a></li>
+                            <li><a href="{{ route('backend.product.index') }}">Products</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="{{ route('backend.supplier.index') }}"><i class="fa fa-university"></i><span class="nav-label">Supplier</span></a></li>
+                    <li>
+                        <a href="#"><i class="fa fa-users"></i><span class="nav-label">Customer</span></a>
+                        <ul class="nav nav-third-level">
+                            <li><a href="#">Point Logs</a></li>
+                        </ul>
+                    </li>
+                     <li><a href="#"><i class="fa fa-briefcase"></i><span class="nav-label">Transactions</span></a></li>
                 </ul>
             </li>
             <li class="@if($nav_active=='customer') active @endif">
-                <a href="#"><i class="fa fa-users"></i> <span class="nav-label">Customer</span></a>
+                <a href="#"><i class="fa fa-cogs"></i> <span class="nav-label">Settings</span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="{{ route('backend.category.index') }}"><i class="fa fa-tags"></i> <span class="nav-label">Categories</span></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-credit-card"></i> <span class="nav-label">Payment Methods</span></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-truck"></i> <span class="nav-label">Couriers</span></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-home"></i> <span class="nav-label">Store</span></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-life-ring"></i> <span class="nav-label">Policies</span></a>
+                    </li>
+                </ul>
             </li>
             <li class="@if($nav_active=='storage') active @endif">
-                <a href="#"><i class="fa fa-archive"></i> <span class="nav-label">Inventory</span></a>
-            </li>
-            <li class="@if($nav_active=='transaction') active @endif">
-                <a href="#"><i class="fa fa-briefcase"></i> <span class="nav-label">Transaksi</span><span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-file-text-o"></i> <span class="nav-label">Report</span></a>
                 <ul class="nav nav-second-level">
-                    <li><a href="#">Penjualan</a></li>
-                    <li><a href="#">Kupon</a></li>
-                </ul>
-            </li>
-            <li class="@if($nav_active=='payment') active @endif">
-                <a href="#"><i class="fa fa-money"></i> <span class="nav-label">Pembayaran</span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li><a href="#">Validasi Pembayaran</a></li>
-                    <li><a href="#">Data Pembayaran</a></li>
-                </ul>
-            </li>
-            <li class="@if($nav_active=='shipping') active @endif">
-                <a href="#"><i class="fa fa-truck"></i> <span class="nav-label">Pengiriman Barang</span>  <span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li><a href="#">Kirim Barang</a></li>
-                    <li><a href="#">Data Pengirimin</a></li>
-                    <li><a href="#">Kurir</a></li>
-                </ul>
-            </li>
-            <li class="@if($nav_active=='report') active @endif">
-                <a href="#"><i class="fa fa-file"></i> <span class="nav-label">Laporan</span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li><a href="#">Laporan Penjualan</a></li>
-                    <li><a href="#">Laporan Kupon</a></li>
-                    <li><a href="#">Laporan Stock</a></li>
-                </ul>
-            </li>
-            <li class="@if($nav_active=='settings') active @endif">
-                <a href="#"><i class="fa fa-globe"></i> <span class="nav-label">Settings</span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li><a href="#">Homepage Config</a></li>
-                </ul>
-            </li>
-            <li class="@if($nav_active=='dashboard') active @endif">
-                <a href="{{ route('backend.supplier.index') }}"><i class="fa fa-university"></i> <span class="nav-label">Supplier</span></a>
-            </li>
-            <li class="@if($nav_active=='product') active @endif">
-                <a href="#"><i class="fa fa-archive"></i> <span class="nav-label">Product</span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li class="@if(isset($subnav_active)&&($subnav_active=='category')) active @endif"><a href="{{ route('backend.category.index') }}">Category</a></li>
-                    <li class="@if(isset($subnav_active)&&($subnav_active=='product')) active @endif"><a href="{{ route('backend.product.index') }}">Product</a></li>
-                    <li><a href="{{ route('backend.price.index') }}">Price</a></li>
-                    <li><a href="{{ route('backend.discount.index') }}">Discount</a></li>
+                    <li>
+                        <a href=""><i class="fa fa-briefcase"></i> <span class="nav-label">Transactions</span></a>
+                        <ul class="nav nav-third-level">
+                            <li><a href="">New Orders</a></li>
+                            <li><a href="">Awaiting Payments</a></li>
+                            <li><a href="">Awaiting Shipping</a></li>
+                            <li><a href="">Complete Orders (Delivered)</a></li>
+                            <li><a href="">Purchasing Stocks</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-line-chart"></i> <span class="nav-label">Stocks Movement</span></a>
+                    </li>
+                     <li>
+                        <a href=""><i class="fa fa-exclamation-circle"></i> <span class="nav-label">Critical Stocks</span></a>
+                    </li>
                 </ul>
             </li>
         </ul>
