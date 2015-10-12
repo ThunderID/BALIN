6fe47ba615337dee72db41f50157927409aa9324
budi7 <budi-purnomo@outlook.com>
2015-10-12 11:45:43 +0700

    add job generate refferal code

---
+++ app/Jobs/GenerateRefferalCode.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use App\Models\user;
+use App\Libraries\JSend;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+
+class GenerateRefferalCode extends Job implements SelfHandling
+{
+    use DispatchesJobs, ValidatesRequests;
+
+    protected $user;
+
+    public function __construct(user $user)
+    {
+        $this->user             = $user;
+    }
+
+    public function handle()
+    {
+        // checking
+        if(is_null($this->user->id))
+        {
+            throw new Exception('Sent variable must be object of a record.');
+        }
+
+        //check is user doesnt have refferal code yet
+        if($this->user['referral_code'])
+        {
+            return new Jsend('error', (array)$this->user, 'Referral code allready exist');
+        }
+        else
+        {
+            $transac_data           = $this->dispatch(new CheckIsCustomerHasProcessedTransaction($this->user));
+
+            if($transac_data->getstatus() == 'success')
+            {
+                //generate refferal code
+                $name               = preg_split('/\s+/', $this->user['name']);
+                $uid                = $this->user['id'];
+
+                $ref_code           = $name[0] . $uid;
+
+                $data               = $this->user;
+
+                $data->fill([
+                    'referral_code' => $ref_code
+                ]);
+
+                if($data->save())
+                {
+                    $result         = new Jsend('success', (array)$data);
+                }
+                else
+                {
+                    $result         = new Jsend('error', (array)$this->user, (array)$data->getError());
+                }
+
+                return $result;
+            }
+            else
+            {
+                return $transac_data;
+            }            
+        }
+    }
+}
