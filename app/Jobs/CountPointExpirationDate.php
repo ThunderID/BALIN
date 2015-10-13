b4ef9e40e5fe8d6872b134599f15c2d44d309e28
budi7 <budi-purnomo@outlook.com>
2015-10-13 15:17:39 +0700

    add job count point expiration date

---
+++ app/Jobs/CountPointExpirationDate.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use App\Models\User;
+use App\Models\policy;
+use App\Libraries\JSend;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class CountPointExpirationDate extends Job implements SelfHandling
+{
+    protected $user;
+
+    public function __construct(user $user)
+    {
+        $this->user                 = $user;
+    }
+
+
+    public function handle()
+    {
+        if(is_null($this->user->id))
+        {
+            throw new Exception('Sent variable must be object of a record.');
+        }
+
+        try 
+        {
+            $join_date                  = $this->user['joined_at'];
+
+            $expired_range              = Policy::type('reset_point')->ondate('now')->first();
+
+            $expired_date               = date('Y-m-d H:i:s', strtotime($join_date . $expired_range['value']));
+
+            do {
+                $expired_date           = date('Y-m-d H:i:s', strtotime($expired_date   . $expired_range['value']));
+            } while ($expired_date < date('Y-m-d H:i:s'));
+
+            $result                     = new Jsend('success', (array)$expired_date);
+
+        } 
+        catch (Exception $e) 
+        {
+            $result                     = new Jsend('fail', (array)$e);
+        }
+
+        return $result;
+    }
+}
