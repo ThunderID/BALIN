28da26d4452f36bb0d453a47b8d0dfb6e0876447
budi7 <budi-purnomo@outlook.com>
2015-10-16 14:53:19 +0700

    update base controller

--- app/Http/Controllers/baseController.php
+++ app/Http/Controllers/baseController.php
@@ -1,11 +1,13 @@
 <?php namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller;
+use Illuminate\Routing\Controller as Controller;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class baseController extends Controller {
 	protected $layout;
+
+    use DispatchesJobs, ValidatesRequests;
 
 	function __construct() 
 	{
