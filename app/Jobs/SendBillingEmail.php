55a90493284fecb9ae65801d3a9c6fe9b76a3b42
budi7 <budi-purnomo@outlook.com>
2015-10-17 12:49:28 +0700

    fix send billing email return

--- app/Jobs/SendBillingEmail.php
+++ app/Jobs/SendBillingEmail.php
@@ -6,10 +6,14 @@ use App\Jobs\Job;
 use App\Models\Transaction;
 
 use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
-class SendBillingEmail extends Job implements SelfHandling
+use App\Libraries\JSend;
+
+
+class SendBillingEmail extends Job implements SelfHandling, ShouldQueue
 {
     use DispatchesJobs;
 
@@ -22,27 +26,34 @@ class SendBillingEmail extends Job implements SelfHandling
 
 	public function handle()
 	{
-	    // checking
-	    if(is_null($this->transaction->id))
-	    {
-	        throw new Exception('Sent variable must be object of a record.');
-	    }
-	    
-	    //get Billing
-		$datas 								= $this->dispatch(new GenerateBillingEmail($this->transaction));        
+		try
+		{
+		    // checking
+		    if(is_null($this->transaction->id))
+		    {
+		        throw new Exception('Sent variable must be object of a record.');
+		    }
+		    
+		    //get Billing
+			$datas 								= $this->dispatch(new GenerateBillingEmail($this->transaction));        
 
-        //send email
-        $mail_data      = [
-                            'view'          => 'emails.test', 
-                            'datas'         => (array)$datas, 
-                            'dest_email'    => 'budi-purnomo@outlook.com', 
-                            'dest_name'     => 'budi purnomo', 
-                            'subject'       => 'Billing Information', 
-                        ];
+	        //send email
+	        $mail_data      = [
+	                            'view'          => 'emails.test', 
+	                            'datas'         => (array)$datas, 
+	                            'dest_email'    => 'budi-purnomo@outlook.com', 
+	                            'dest_name'     => 'budi purnomo', 
+	                            'subject'       => 'Billing Information', 
+	                        ];
 
-        // call email send job
-        $this->dispatch(new Mailman($mail_data));
+	        // call email send job
+	        $this->dispatch(new Mailman($mail_data));
 
-        return true;
-	}
+	         return new Jsend('success', (array)$transaction);
+		}
+		catch (Exception $e) 
+		{
+		    $this->release(10);
+		}		
+	}	
 }
