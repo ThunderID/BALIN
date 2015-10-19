4f5b229480fd56ecafbdd6a85c0a6d88688f69d6
budi7 <budi-purnomo@outlook.com>
2015-10-19 17:31:45 +0700

    working job calculate transaction ammount

---
+++ app/Jobs/CalculateTransactionAmount.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+
+use App\Models\transaction;
+use App\Models\transactiondetail;
+
+use App\Libraries\JSend;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class CalculateTransactionAmount extends Job implements SelfHandling
+{
+    
+    Protected $transaction;
+
+    public function __construct(transaction $transaction)
+    {
+        $this->transaction                  = $transaction;
+    }
+
+    public function handle()
+    {
+        if(is_null($this->transaction->id))
+        {
+            throw new Exception('Sent variable must be object of a record.');
+        }
+
+        $uniq_number                        = 123;
+
+        $product_prices                     = transactiondetail::where('transaction_id', $this->transaction->id)
+                                                ->selectraw('(price - discount) * quantity as total')
+                                                ->first()
+                                                ;
+
+        $amount                            = ($this->transaction->shipping_cost + $product_prices['total']) - ($this->transaction->uniq_number + $this->transaction->shipping_cost);
+
+        $this->transaction->fill([
+            'amount'         => $amount,
+        ]);
+
+        if($this->transaction->save())
+        {
+            return true;
+            // dd('saved');
+        }
+        else
+        {
+            return true;
+            // dd('error');
+        }
+    }
+}
