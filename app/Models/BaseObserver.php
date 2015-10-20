fef09a783b497a46ace2561553751314d4bf3786
budi7 <budi-purnomo@outlook.com>
2015-10-20 11:17:54 +0700

    base observer update

--- app/Models/BaseObserver.php
+++ app/Models/BaseObserver.php
@@ -15,12 +15,32 @@ class BaseObserver
 
 	public function saved($model)
 	{
+
+		$class 			= $this->before($model);
+
+		return $this->after($class, $model);
+	}
+	
+	public function Saving($model)
+	{
+		$class 			= $this->before($model);
+
+		return $this->after($class, $model);
+	}
+
+	public function before($model)
+	{
 		$class 			= get_class($model);
 		$class2 		= explode( '\\', $class );
 		$lastnameclass 	= end( $class2 );
 		$class 			= str_replace('Models', 'Jobs', $class);
-		$class 			= str_replace($lastnameclass, $lastnameclass.'Saved', $class);
+		$class 			= str_replace($lastnameclass, $lastnameclass.debug_backtrace()[1]['function'], $class);
 
+		return $class;	
+	}
+
+	public function after($class, $model)
+	{
 		if (class_exists($class) && get_parent_class($class) == 'App\Jobs\Job') 
 		{
 	        $result                         = $this->dispatch(new $class($model));
@@ -39,9 +59,7 @@ class BaseObserver
 	    else
 	    {
 	        trigger_error("Unable to load class: $class", E_USER_WARNING);
-	    	
 	    }
-
 
         return false;
 	}
