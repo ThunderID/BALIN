0d37d0e19e15e448411f9d0a47190c8233a42bf1
budi7 <budi-purnomo@outlook.com>
2015-10-20 16:30:06 +0700

    fix job transactioncreating, transactiondetailsaved, transactiondetailsaving

---
+++ app/Jobs/TransactionCreating.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Libraries\JSend;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+use App\Jobs\Job;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use App\Models\Transaction;
+
+
+class TransactionCreating extends Job implements SelfHandling
+{
+    use DispatchesJobs, ValidatesRequests;
+
+    protected $transaction;
+
+    public function __construct(transaction $transaction)
+    {
+        $this->transaction                  = $transaction;
+    }
+
+    public function handle()
+    {
+        $result                             = $this->dispatch(new generateTransactionUniqNumber($this->transaction));
+
+        return $result;
+    }
+}
---
+++ app/Jobs/TransactionDetailSaved.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use App\Libraries\JSend;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+use App\Models\transaction;
+use App\Models\transactiondetail;
+
+class TransactionDetailSaved extends Job implements SelfHandling
+{
+    use DispatchesJobs, ValidatesRequests;
+
+    protected $transactiondetail;
+
+    public function __construct(transactiondetail $transactiondetail)
+    {
+        $this->transactiondetail            = $transactiondetail;
+    }
+
+    public function handle()
+    {
+        $transaction                        = transaction::find($this->transactiondetail->transaction_id);
+    
+        $result                             = $this->dispatch(new calculateTransactionAmount($transaction));
+
+        return $result;
+    }
+}
