0c8d11cf4c50a2a7bd2bc456f6ff8de15b5ffccd
budi7 <budi-purnomo@outlook.com>
2015-10-22 16:20:03 +0700

    update product deleting

--- app/Jobs/ProductDeleting.php
+++ app/Jobs/ProductDeleting.php
@@ -23,9 +23,9 @@ class ProductDeleting extends Job implements SelfHandling
     {
         if($this->product->transactiondetails->count())
         {
-            return new jsend('error', (array)$this->product, ['message' => 'Produk memiliki transaksi'])
+            return new jsend('error', (array)$this->product, ['message' => 'Produk memiliki transaksi']);
         }
 
-        return new jsend('success', (array)$this->product)
+        return new jsend('success', (array)$this->product);
     }
 }
