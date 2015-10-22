d3e1c64edda391ea9d375d4a3e8040c086d6580a
budi7 <budi-purnomo@outlook.com>
2015-10-22 13:52:00 +0700

    revise morph table image

--- database/migrations/2015_06_19_034756_create_image_table.php
+++ database/migrations/2015_06_19_034756_create_image_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductImageTable extends Migration
+class CreateImageTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,11 @@ class CreateProductImageTable extends Migration
         Schema::create('product_images', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->unsigned()->index();
+            $table->string('product_type', 255);
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('user_type', 255);
+            $table->integer('courier_id')->unsigned()->index();
+            $table->string('courier_type', 255);
             $table->string('thumbnail', 255);
             $table->string('image_xs', 255);
             $table->string('image_sm', 255);
