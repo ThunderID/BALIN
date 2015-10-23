73bb54abc3a69aefb805536d14476207f00e250b
uchuy77 <agil.mahendra@gmail.com>
2015-10-23 11:33:34 +0700

    add & modified select2

--- resources/views/plugins/select2.blade.php
+++ resources/views/plugins/select2.blade.php
@@ -123,4 +123,104 @@
 		}
 	});
 	$('.select-product-by-name').select2('data', preload_data );
+
+	$('.select-method-transaction').on('change', function() {
+		var value = $(this).val();
+		if (value==='point_log') {
+			console.log('yes');
+			$('div.no-rek').hide();
+			$('div.point').show();
+		}
+		else 
+		{
+			console.log('no');
+			$('div.point').hide();
+			$('div.no-rek').show();
+		}
+
+	});
+
+	$('.select-courier-by-name').select2({
+		placeholder: 'Masukkan nama Courier',
+		minimumInputLength: 2,
+		maximumSelectionSize: 1,
+		tags: false,
+		ajax : {
+			url: "{{ route('backend.courier.ajax.getCourierByName') }}",
+			dataType: 'json',
+			data: function (term, path) {
+				return {
+					name: term,
+					path : '{{ isset($data['path']) ? $data['path'] : '' }}'
+				};
+			},
+		   results: function (data) {
+				return {
+					results: $.map(data, function (item) {
+						return {
+							text: item.name +' ',
+							id: item.id +' ',
+							path: item.path
+						}
+						$('.price').val(item.price);
+						// $(this).attr('data-price', item.price);
+					})
+				};
+			},
+			query: function (query){
+				var data = {results: []};
+				 
+				$.each(preload_data, function(){
+					if(query.term.length == 0 || this.text.toUpperCase().indexOf(query.term.toUpperCase()) >= 0 ){
+						data.results.push({id: this.id, text: this.text });
+					}
+				});
+	
+				query.callback(data);
+			}	
+		}
+	});
+	$('.select-courier-by-name').select2('data', preload_data );
+
+	$('.select-supplier-by-name').select2({
+		placeholder: 'Masukkan nama Supplier',
+		minimumInputLength: 2,
+		maximumSelectionSize: 1,
+		tags: false,
+		ajax : {
+			url: "{{ route('backend.supplier.ajax.getSupplierByName') }}",
+			dataType: 'json',
+			data: function (term, path) {
+				return {
+					name: term,
+					path : '{{ isset($data['path']) ? $data['path'] : '' }}'
+				};
+			},
+		   results: function (data) {
+				return {
+					results: $.map(data, function (item) {
+						return {
+							text: item.name +' ',
+							id: item.id +' ',
+							path: item.path
+						}
+						$('.price').val(item.price);
+						// $(this).attr('data-price', item.price);
+					})
+				};
+			},
+			query: function (query){
+				var data = {results: []};
+				 
+				$.each(preload_data, function(){
+					if(query.term.length == 0 || this.text.toUpperCase().indexOf(query.term.toUpperCase()) >= 0 ){
+						data.results.push({id: this.id, text: this.text });
+					}
+				});
+	
+				query.callback(data);
+			}	
+		}
+	});
+	$('.select-supplier-by-name').select2('data', preload_data );
 </script>
