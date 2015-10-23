e092dee1bdf0451bbe5174c8c412db2a0863d979
chelsymooy <chelsymooy1108@gmail.com>
2015-10-23 09:25:41 +0700

    repair product show

--- resources/views/pages/backend/data/product/show.blade.php
+++ resources/views/pages/backend/data/product/show.blade.php
@@ -1,7 +1,6 @@
 @inject('data', 'App\Models\Product')
 <?php $data = $data::where('id', $id)
 									->with('categories')
-									// ->with(['productattributes' => function($q){$q->orderBy('attribute', 'asc');}])
 									->with('images')
 									->first(); 
 
@@ -18,102 +17,40 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-6 col-sm-6 col-xs-12">
-			<div class="form-group">
-				<label for="name">Nama</label>
-				{!! Form::text('name', $data['name'], ['class' => 'form-control mod_name', 'required' => 'required', 'tabindex' => '1', 'readonly' => 'readonly'] ) !!}
-			</div>
-		</div>
-		<div class="col-md-6 col-sm-6 col-xs-12">		
-			<label for="sku">SKU</label>
-			{!! Form::text('sku', $data->sku, ['class' => 'form-control mod_name', 'required' => 'required', 'tabindex' => '1', 'readonly' => 'readonly'] ) !!}
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-6 col-sm-6 col-xs-12">
-			<div class="form-group">
-				<label for="">Kondisi</label>
-				<input type="text" value="@if($data->is_new==1) Baru @else Bekas @endif" class="form-control" readonly />
-			</div>
-		</div>
-		<div class="col-md-6 col-sm-6 col-xs-12">
-			<div class="form-group">
-				<label for="">Stok</label>
-				{!! Form::text('stock', $data->stock, ['class' => 'form-control', 'tabindex' => '1', 'readonly' => 'readonly'] ) !!}
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12 col-sm-12 col-xs-12">
-			<div class="form-group">
-				<label for="">Deskripsi</label>
-				{!! Form::textarea('description', $data->description, ['class' => 'form-control', 'rows' => '3', 'tabindex' => '1', 'style' => 'resize:none;', 'readonly' => 'readonly'] ) !!}
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-4 col-sm-4 col-xs-12">
-			<div class="form-group">
-				<label for="">Harga</label>
-				{!! Form::text('price', $data->price, ['class' => 'form-control', 'tabindex' => '1', 'readonly' => 'readonly']) !!}
-			</div>
-		</div>
-		<div class="col-md-4 col-sm-4 col-xs-12">
-			<div class="form-group">
-				<label for="">Harga Promo</label>
-				{!! Form::text('promo_price', $data->promo_price, ['class' => 'form-control', 'tabindex' => '1', 'readonly' => 'readonly'] ) !!}
-			</div>
-		</div>
-		<div class="col-md-4 col-sm-4 col-xs-12">
-			<div class="form-group">
-				<label for="">Diskon</label>
-				{!! Form::text('discount', $data->discount, ['class' => 'form-control', 'tabindex' => '1', 'readonly' => 'readonly'] ) !!}
-			</div>
-		</div>
-	</div>
-
-	<div class="clearfix">&nbsp;</div>
-	<div class="clearfix">&nbsp;</div>
-
-	<div class="row">
-		<div class="col-md-12">
-			<h4 class="sub-header">
-				Kategori
-			</h4>
-		</div>
-	</div>
-	@foreach ($data->categories as $cat)
-		<div class="row">
-			<div class="col-md-12 col-sm-12 col-xs-12">
-				<div class="form-group">
-					{!! Form::text('categories', $cat->name, ['class' => 'form-control', 'tabindex' => '1', 'readonly' => 'readonly'] ) !!}
+		<div class="col-md-4">
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-xs-12 text-center">
+					@foreach ($data->images as $k => $img)
+						@if ($k==5)
+								</div>
+							</div>
+							<div class="clearfix">&nbsp;</div>
+							<div class="row">
+								<div class="col-md-12 col-sm-12 col-xs-12 text-center">
+						@endif
+						{!! HTML::image($img->image_xs) !!}
+					@endforeach
 				</div>
 			</div>
 		</div>
-	@endforeach
-
-	<div class="clearfix">&nbsp;</div>
-	<div class="clearfix">&nbsp;</div>
-
-	<div class="row">
-		<div class="col-md-12">
-			<h4 class="sub-header">
-				Images
-			</h4>
+		<div class="col-md-8">
+			<h2 style="margin-top:0px;">{!!$data->name!!}</h2>
+			<h5><strong>SKU</strong> {!!$data->sku!!}</h5>
+			<h5><strong>Stok</strong> {!!$data->stock!!}</h5>
+			<h5><strong>Harga</strong> @if($data->discount!=0)<strike> {!!$data->price!!} </strike> {!!$data->promo_price!!} @else {!!$data->price!!} @endif</h5>
+			<h5><strong>Diskon</strong> {!!$data->discount!!}</h5>
+			<i class = "fa fa-tags"></i>
+			@foreach($data->categories as $key => $value)
+				@if($key!=0)
+					,
+				@endif
+				{!! $value->name !!}
+			@endforeach
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-12 col-sm-12 col-xs-12 text-center">
-			@foreach ($data->images as $k => $img)
-				@if ($k==5)
-						</div>
-					</div>
-					<div class="clearfix">&nbsp;</div>
-					<div class="row">
-						<div class="col-md-12 col-sm-12 col-xs-12 text-center">
-				@endif
-				{!! HTML::image($img->image_xs) !!}
-			@endforeach
+		<div class="col-md-12">
+			{!!$data->description!!}
 		</div>
 	</div>
 
