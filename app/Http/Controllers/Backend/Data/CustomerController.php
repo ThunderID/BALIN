a425885a347f39ae1e4df1bd2a6de43c2cdbfb74
chelsymooy <chelsymooy1108@gmail.com>
2015-10-24 12:47:17 +0700

    Bring back customer controller and mail activation

--- app/Http/Controllers/Backend/Data/CustomerController.php
+++ app/Http/Controllers/Backend/Data/CustomerController.php
@@ -1,159 +1,187 @@
-<?php namespace App\Http\Controllers\Backend\Data;
+<?php 
+namespace App\Http\Controllers\Backend\Data;
 
 use App\Http\Controllers\baseController;
 use App\Models\User;
 use Input, Session, DB, Redirect, Response;
 
-class customerController extends baseController 
+class CustomerController extends baseController
 {
-	protected $view_name 					= 'Customer';
+	/**
+	* Instantiate a new UserController instance.
+	*/
+	public function __construct()
+	{
+		$this->middleware('passwordneeded', ['only' => ['destroy']]);.
 
+		parent::__construct();
+	}
+
+	protected $view_name = 'Customer';
+	
 	public function index()
-	{		
-		$breadcrumb								= ['Customer' => 'backend.data.customer.index'];
+	{
+		$breadcrumb				= ['Customer' => 'backend.data.customer.index'];
 
-		if (Input::get('q'))
+		$filters 				= ['customer' => true];
+
+		if(Input::has('q'))
 		{
-			$searchResult						= Input::get('q');
+			$filters['name'] 	= Input::get('q');
+
+			$searchResult		= Input::get('q');
 		}
 		else
 		{
-			$searchResult						= NULL;
+			$searchResult		= null;
 		}
 
-		$this->layout->page 					= view('pages.backend.data.user.index')
-														->with('WT_pageTitle', $this->view_name )
-														->with('WT_pageSubTitle','Index')
-														->with('WB_breadcrumbs', $breadcrumb)
-														->with('searchResult', $searchResult)
-														->with('nav_active', 'data')
-														->with('subnav_active', 'customer');
-		return $this->layout;		
+		$this->layout->page 	= view('pages.backend.data.user.index')
+									->with('WT_pageTitle', $this->view_name )
+									->with('WT_pageSubTitle','Index')
+									->with('WB_breadcrumbs', $breadcrumb)
+									->with('searchResult', $searchResult)
+									->with('filters', $filters)
+									->with('nav_active', 'data')
+									->with('subnav_active', 'customer');
+		return $this->layout;
+	}
+
+	public function show($id)
+	{
+		$breadcrumb				= 	[ 	'Kostumer' => 'backend.data.customer.index',
+										'Detail' => 'backend.data.customer.create',
+									];
+
+		if(Input::has('q'))
+		{
+			$searchResult			= $search;
+		}
+		else
+		{
+			$searchResult			= NULL;
+		}
+
+		$this->layout->page 		= view('pages.backend.data.user.show')
+									->with('WT_pageTitle', $this->view_name )
+									->with('WT_pageSubTitle','Show')
+									->with('WB_breadcrumbs', $breadcrumb)
+									->with('searchResult', $searchResult)
+									->with('id', $id)
+									->with('nav_active', 'data')
+									->with('subnav_active', 'customer')
+		;
+
+		return $this->layout;
 	}
 
 	public function create($id = null)
 	{
-		if (!$id)
+		if (is_null($id))
 		{
-			$breadcrumb							= ['Customer' => 'backend.data.customer.index',
-														'Customer Baru' => 'backend.data.customer.create' ];
+			$breadcrumb				= 	[ 	'Customer' => 'backend.data.customer.index',
+											'Customer Baru' => 'backend.data.customer.create'
+			];
 		}
 		else
 		{
-			$breadcrumb							= ['Customer' => 'backend.data.customer.index',
-														'Edit Data' => 'backend.data.customer.create' ];
+			$breadcrumb				= 	[ 	'Customer' => 'backend.data.customer.index',
+											'Edit Data' => 'backend.data.customer.create'
+										];
 		}
-
-		$this->layout->page 					= view('pages.backend.data.user.create')
-															->with('WT_pageTitle', $this->view_name )
-															->with('WT_pageSubTitle','Create')		
-															->with('WB_breadcrumbs', $breadcrumb)
-															->with('id', $id)
-															->with('nav_active', 'data')
-															->with('subnav_active', 'customer');
-		return $this->layout;		
+		
+		$this->layout->page 		= view('pages.backend.data.user.create')
+										->with('WT_pageTitle', $this->view_name )
+										->with('WT_pageSubTitle','Create')
+										->with('WB_breadcrumbs', $breadcrumb)
+										->with('id', $id)
+										->with('nav_active', 'data')
+										->with('subnav_active', 'customer');
+		return $this->layout;
 	}
 
 	public function edit($id)
 	{
-
 		return $this->create($id);
 	}
 
 	public function store($id = null)
 	{
-		$inputs 								= Input::only('id','name', 'phone', 'address', 'email', 'postal_code', 'role');
-	
-		if ($inputs['id'])
+		$inputs 					= Input::only('name', 'phone', 'address', 'email', 'postal_code');
+		
+		if(!is_null($id))
 		{
-			$data								= user::find($inputs['id']);
+			$data					= User::find($id);
 		}
 		else
 		{
-			$data								= new user;
+			$data					= new User;
 		}
 
 		$data->fill([
-			'name' 							=> $inputs['name'],
-			'phone' 							=> $inputs['phone'],
-			'address' 						=> $inputs['address'],
-			'email'							=> $inputs['email'],
-			'postal_code'					=> $inputs['postal_code'],
-			'role'							=> $inputs['role'],
+				'name' 				=> $inputs['name'],
+				'phone' 			=> $inputs['phone'],
+				'address' 			=> $inputs['address'],
+				'email'				=> $inputs['email'],
+				'postal_code'		=> $inputs['postal_code'],
+				'role'				=>'customer',
 		]);
 
 		DB::beginTransaction();
+
 		if (!$data->save())
 		{
 			DB::rollback();
+
 			return Redirect::back()
-				->withInput()
-				->withErrors($data->getError())
-				->with('msg-type', 'danger');
-		}	
+					->withInput()
+					->withErrors($data->getError())
+					->with('msg-type', 'danger');
+		}
 		else
 		{
 			DB::commit();
 
-			if(Input::get('id'))
-			{
-				$msg = "Data sudah diperbarui";
-			}
-			else
-			{
-				$msg = "Data sudah ditambahkan";
-			}
-
 			return Redirect::route('backend.data.customer.index')
-				->with('msg',$msg)
+				->with('msg','Kostumer sudah tersimpan')
 				->with('msg-type', 'success');
 		}
 	}
 
+	public function Update($id)
+	{
+		return $this->store($id);
+	}
+
 	public function destroy($id)
 	{
-		if (Input::get('password'))
-		{		
-			$data									= user::find($id);
-
-			if (count($data) == 0)
-			{
-				App::abort(404);
-			}
-
-			DB::beginTransaction();
-
-			if (!$data->delete())
-			{
-				DB::rollback();
-				return Redirect::back()
+		$data = User::findorfail($id);
+		
+		DB::beginTransaction();
+		
+		if (!$data->delete())
+		{
+			DB::rollback();
+			return Redirect::back()
 					->withErrors($data->getError())
 					->with('msg-type','danger');
-			}
-			else
-			{
-				DB::commit();
-				return Redirect::route('backend.data.customer.index')
-					->with('msg', 'Data telah dihapus')
-					->with('msg-type','success');
-			}
 		}
 		else
 		{
-			return Redirect::back()
-					->withErrors('Password tidak valid')
-					->with('msg-type', 'danger');
-		}		
+			DB::commit();
+			return Redirect::route('backend.data.customer.index')
+					->with('msg', 'Data telah dihapus')
+					->with('msg-type','success');
+		}
 	}
 
 	public function getCustomerByName()
 	{
-		$inputs 	= Input::only('name');
-	    
-	    $tmp 		= User::select(['id', 'name'])
-	    						->where('name', 'like', "%" . $inputs['name'] . "%")
-	    						->get();
-	    		
-	    return json_decode(json_encode($tmp));
+		$inputs 			= Input::only('name');
+		$tmp 				= User::select(['id', 'name'])
+								->name($inputs['name'])
+								->get();
+								
+		return json_decode(json_encode($tmp));
 	}
 }
---
+++ app/Jobs/SendActivationEmail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use App\Models\user;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class SendActivationEmail extends Job implements SelfHandling
+{
+    protected $user;
+
+    public function __construct(User $user)
+    {
+        $this->user                         = $user;
+    }
+
+    public function handle()
+    {
+        // checking
+        if(is_null($this->user->id))
+        {
+            throw new Exception('Sent variable must be object of a record.');
+        }
+
+        //send email
+        $mail_data      = [
+                            'view'          => 'emails.activation', 
+                            'datas'         => (array)$this->user->activation_link, 
+                            'dest_email'    => $this->user->email, 
+                            'dest_name'     => $this->user->name, 
+                            'subject'       => '[BALIN] Email Activation', 
+                        ];
+
+        // call email send job
+        $this->dispatch(new Mailman($mail_data));
+
+        return new JSend('success', (array)$this->user);
+    }
+}
