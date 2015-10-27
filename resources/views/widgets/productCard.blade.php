bc8fa712f32ccec0d3769086f93d7297412e7b52
uchuy77 <agil.mahendra@gmail.com>
2015-10-27 16:56:51 +0700

    modified product frontend [beta]

--- resources/views/widgets/productCard.blade.php
+++ resources/views/widgets/productCard.blade.php
@@ -1,15 +1,15 @@
-    <div class="thumbnail">
-        <img class="img img-responsive"  src="{{$data['default_image']}}" alt="">
+<div class="thumbnail">
+	<img class="img img-responsive"  src="{{$data['default_image']}}" alt="">
 
-        <div class="caption">
-            <h4 class="text-center">{{$data['name']}}</h4>
-            @if($data->promo_price!=0)
-	            <p class="text-center normal-price"><strike>Rp {{$data->price}}</strike></p>
-	            <p class="text-center promo-price">Rp {{$data->promo_price}}</p>
-	        @else
-	            <p class="text-center normal-price">Rp {{$data->price}}</p>
-	        @endif
-        </div>
+	<div class="caption m-b-md">
+		<h4 class="text-center">{{$data['name']}}</h4>
+		@if($data->promo_price!=0)
+			<p class="text-center normal-price"><strike>Rp {{$data->price}}</strike></p>
+			<p class="text-center promo-price t-md">Rp {{$data->promo_price}}</p>
+		@else
+			<p class="text-center promo-price t-md">Rp {{$data->price}}</p>
+		@endif
+	</div>
 
-        <a href="{{ route('frontend.product.show', $data['id']) }}" type="button" class="btn btn-default btn-block"><h4>Detail</h4></a>
-    </div>
+	<a href="{{ route('frontend.product.show', $data['id']) }}" type="button" class="btn-hollow hollow-black btn-block t-md">Detail</a>
+</div>
