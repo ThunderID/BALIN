271a0d45a0cc228f8e99afadd4fb667bc11971cb
budi7 <budi-purnomo@outlook.com>
2015-10-27 17:47:04 +0700

    add carousel plugin

---
+++ resources/views/plugins/owlCarousel.blade.php
@@ -0,0 +1,29 @@
+{!! HTML::style('Balin/web/plugin/owlCarousel/assets/owl.carousel.css') !!}
+{!! HTML::script('Balin/web/plugin/owlCarousel/owl.carousel.js') !!}
+
+<script>
+	$(document).ready(function(){
+	  $(".owl-carousel").owlCarousel();
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:true
+	        },
+	        600:{
+	            items:3,
+	            nav:false
+	        },
+	        1000:{
+	            items:5,
+	            nav:true,
+	            loop:false
+	        }
+	    }
+	})
+</script>
