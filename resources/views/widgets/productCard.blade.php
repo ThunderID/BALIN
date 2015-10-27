ac4fc7a26c18173dcad9813fdcef0debed08d899
chelsymooy <chelsymooy1108@gmail.com>
2015-10-27 12:57:50 +0700

    working on pricing

--- resources/views/widgets/productCard.blade.php
+++ resources/views/widgets/productCard.blade.php
@@ -3,8 +3,12 @@
 
         <div class="caption">
             <h4 class="text-center">{{$data['name']}}</h4>
-            <p class="text-center normal-price">Normal Price : {{$data->price}}</p>
-            <p class="text-center promo-price">Promo Price : {{$data->promoprice}}</p>
+            @if($data->promo_price!=0)
+	            <p class="text-center normal-price"><strike>Rp {{$data->price}}</strike></p>
+	            <p class="text-center promo-price">Rp {{$data->promo_price}}</p>
+	        @else
+	            <p class="text-center normal-price">Rp {{$data->price}}</p>
+	        @endif
         </div>
 
         <a href="{{ route('frontend.product.show', $data['id']) }}" type="button" class="btn btn-default btn-block"><h4>Detail</h4></a>
