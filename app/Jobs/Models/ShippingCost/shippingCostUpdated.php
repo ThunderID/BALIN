16fa59292aa5b634e5b9a2e4d9eeaa3b89645905
budi7 <budi-purnomo@outlook.com>
2015-10-27 11:19:31 +0700

    fix shippingcost jobs

---
+++ app/Jobs/Models/ShippingCost/shippingCostUpdated.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use App\Libraries\JSend;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use App\Models\shippingcost;
+
+class shippingCostUpdated extends Job implements SelfHandling
+{
+    protected $shippingcost;
+
+    public function __construct(shippingcost $shippingcost)
+    {
+        $this->shippingcost             = $shippingcost;
+    }
+    
+    public function handle()
+    {
+        $result                     = new JSend('success', (array)$this->supplier);
+        
+        return $result;
+    }
+}
---
+++ app/Jobs/Models/ShippingCost/shippingCostUpdating.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use App\Libraries\JSend;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use App\Models\shippingcost;
+
+class shippingCostUpdating extends Job implements SelfHandling
+{
+    protected $shippingcost;
+
+    public function __construct(shippingcost $shippingcost)
+    {
+        $this->shippingcost             = $shippingcost;
+    }
+    
+    public function handle()
+    {
+        $result                     = new JSend('success', (array)$this->supplier);
+        
+        return $result;
+    }
+}
