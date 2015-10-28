6da15b9eaf5b3bf2ad1916efca3c37486828390f
budi7 <budi-purnomo@outlook.com>
2015-10-28 17:52:53 +0700

    fixed create transaction view

--- resources/views/pages/backend/data/transaction/create.blade.php
+++ resources/views/pages/backend/data/transaction/create.blade.php
@@ -3,8 +3,7 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-12">
-			{!! Form::open(['route' => 'backend.data.transaction.store', 'class' =>'wizard-big', 'id' => 'create-transaction']) !!}
-				{!! Form::hidden('type', Input::get('type')) !!}
+			{!! Form::open(['route' => array('backend.data.transaction.store', 'type' => Input::get('type')), 'class' =>'wizard-big', 'id' => 'create-transaction']) !!}
 				@if ($subnav_active=='sell')
 					@include('pages.backend.data.transaction.sell.create')
 				@else
@@ -13,112 +12,6 @@
 			{!! Form::close() !!}
 		</div>
 	</div>
-	{{-- 
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">	
-					<label for="user_id">User ID</label>
-					{!! Form::text('user_id', null, [
-						'class'         => 'form-control', 
-						'required'      => 'required', 
-						'tabindex'      => '1', 
-						'placeholder'   => ''
-					]) !!}
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">	
-					<label for="courier_id">Courier ID</label>
-					{!! Form::text('courier_id', null, [
-						'class'         => 'form-control', 
-						'required'      => 'required', 
-						'tabindex'      => '1', 
-						'placeholder'   => ''
-					]) !!}
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">	
-					<label for="referral_code">Refferal Code</label>
-					{!! Form::text('referral_code', null, [
-						'class'         => 'form-control', 
-						'tabindex'      => '1', 
-						'placeholder'   => ''
-					]) !!}
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">	
-					<label for="type">Type</label>
-					{!! Form::text('type', null, [
-						'class'         => 'form-control', 
-						'required'      => 'required', 
-						'tabindex'      => '1', 
-						'placeholder'   => ''
-					]) !!}
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">	
-					<label for="status">Status</label>
-					{!! Form::text('status', null, [
-						'class'         => 'form-control', 
-						'required'      => 'required', 
-						'tabindex'      => '1', 
-						'placeholder'   => ''
-					]) !!}
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">	
-					<label for="shipping_cost">Shipping Cost</label>
-					{!! Form::text('shipping_cost', null, [
-						'class'         => 'form-control', 
-						'required'      => 'required', 
-						'tabindex'      => '1', 
-						'placeholder'   => ''
-					]) !!}
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">	
-					<label>Produk</label>
-					{!! Form::text('products[0][id]', null, [
-						'class'         => 'form-control', 
-						'required'      => 'required', 
-						'tabindex'      => '1', 
-						'placeholder'   => ''
-					]) !!}
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">	
-					<label>QTY</label>
-					{!! Form::text('products[0][quantity]', null, [
-						'class'         => 'form-control', 
-						'required'      => 'required', 
-						'tabindex'      => '1', 
-						'placeholder'   => ''
-					]) !!}
-				</div>
-			</div>
-		</div>		 --}}
-		{{-- <div class="row">
-			<div class="col-md-12">
-				<div class="clearfix">&nbsp;</div>
-				<div class="form-group text-right">
-					<a href="{{ URL::route('backend.data.transaction.index') }}" class="btn btn-md btn-default" tabindex="6">Batal</a>
-					<button type="submit" class="btn btn-md btn-success" tabindex="7">Simpan</button>
-				</div>        
-			</div>        
-		</div> 		 --}}
-	{{-- {!! Form::close() !!} --}}
 @stop   
 
 @section('script')
