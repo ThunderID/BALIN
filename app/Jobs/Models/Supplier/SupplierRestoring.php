796a1a89593a50d6900c65886df4d2b8f5d56bba
budi7 <budi-purnomo@outlook.com>
2015-10-30 13:33:58 +0700

    [#sp1] add jobs supplier

---
+++ app/Jobs/Models/Supplier/SupplierRestoring.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use App\Libraries\JSend;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use App\Models\Supplier;
+
+class SupplierRstoring extends Job implements SelfHandling
+{
+    use DispatchesJobs, ValidatesRequests;
+
+    protected $supplier;
+
+    public function __construct(Supplier $supplier)
+    {
+        $this->supplier             = $supplier;
+    }
+
+    public function handle()
+    {
+        //cek 
+        if($this->supplier->transactions()->count())
+        {
+            $result                 = new JSend('error', (array)$this->supplier, 'Tidak dapat menghapus supplier yang pernah melakukan transaksi');
+        }
+        else
+        {
+            $result                 = new JSend('success', (array)$this->supplier);
+        }
+
+        return $result;
+    }
+}
