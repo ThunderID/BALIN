5127bbd00eba526745f9246aa2c954ac1c5d5413
uchuy77 <agil.mahendra@gmail.com>
2015-10-30 08:56:28 +0700

    add mask harga in product create

--- resources/views/plugins/input-mask.blade.php
+++ resources/views/plugins/input-mask.blade.php
@@ -1,5 +1,6 @@
-{!! HTML::script('Balin/admin/plugin/input-mask/inputmask.min.js') !!}
+{!! HTML::script('Balin/admin/plugin/input-mask/jquery.mask.min.js') !!}
 
 <script>
-	
+	$('.money').mask('000.000.000.000.000', {reverse: true});
+	$('.date_time').mask('00/00/0000 00:00:00');
 </script>
