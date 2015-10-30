1762850d103f6e20555765aaa8c302b9588c1070
budi7 <budi-purnomo@outlook.com>
2015-10-30 14:08:39 +0700

    [PR1]price add job

---
+++ app/Jobs/Models/Price/PriceRestored.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Jobs\Models\Price;
+
+use App\Jobs\Job;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use App\Libraries\JSend;
+
+use App\Models\Price;
+
+class PriceRestored extends Job implements SelfHandling
+{
+    protected $price;
+
+    public function __construct(Price $price)
+    {
+        $this->price                    = $price;
+    }
+
+    public function handle()
+    {
+        return new JSend('success', (array)$this->price);
+    }
+}
