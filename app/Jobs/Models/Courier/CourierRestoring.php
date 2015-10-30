5185359402844e148bc81acf3f97157851973f4a
budi7 <budi-purnomo@outlook.com>
2015-10-30 13:23:03 +0700

    [cs1] courier add job

---
+++ app/Jobs/Models/Courier/CourierRestoring.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Jobs\Models\Courier;
+
+use App\Jobs\Job;
+use App\Libraries\JSend;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use App\Models\Courier;
+
+class CourierRestoring extends Job implements SelfHandling
+{
+    protected $courier;
+
+    public function __construct(Courier $courier)
+    {
+        $this->courier             = $courier;
+    }
+
+    public function handle()
+    {
+        return new JSend('success', (array)$this->courier);
+    }
+}
