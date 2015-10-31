2fb5dfa0757a88f846ffadf69b68641238fab85a
budi7 <budi-purnomo@outlook.com>
2015-10-31 10:02:58 +0700

    [#AD3] address jobs

---
+++ app/Jobs/Models/Address/AddressUpdating.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use App\Libraries\JSend;
+
+use App\Models\Addres;
+
+class AddressUpdating extends Job implements SelfHandling
+{
+    public function __construct(address $address)
+    {
+        $this->address              = $address;
+    }
+
+    public function handle()
+    {
+        return new JSend('success', (array)$this->courier);
+    }
+}
