28063dc83127eab4eab7dc8effb059c8f1a36e1b
chelsymooy <chelsymooy1108@gmail.com>
2015-11-04 09:03:16 +0700

    Add quota log table

---
+++ app/Models/QuotaLog.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class QuotaLog extends Eloquent
+{
+
+	use SoftDeletes;
+
+	/**
+	 * The trait used by the model.
+	 *
+	 * @var string
+	 */
+
+	use \App\Models\Traits\belongsTo\HasUserTrait;
+	use \App\Models\Traits\morphTo\HasReferenceTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table				= 'point_logs';
+
+	// protected $timestamps			= true;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+
+	protected $fillable				=	[
+											'user_id'						,
+											'reference_id'					,
+											'reference_type'				,
+											'amount'						,
+											'notes'							,
+										];
+
+	/**
+	 * Timestamp field
+	 *
+	 * @var array
+	 */
+	protected $dates				=	['created_at', 'updated_at', 'deleted_at'];
+
+	/**
+	 * Basic rule of database
+	 *
+	 * @var array
+	 */
+	protected $rules				=	[
+											'amount'						=> 'numeric',
+										];
+
+	/**
+	 * The appends attributes from mutator and accessor
+	 *
+	 * @var array
+	 */
+	protected $appends				=	[
+										];
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden 				= [];
+
+	
+	/* ---------------------------------------------------------------------------- MUTATOR ---------------------------------------------------------------------------------*/
+
+	/* ---------------------------------------------------------------------------- ACCESSOR --------------------------------------------------------------------------------*/
+
+	/* ---------------------------------------------------------------------------- FUNCTIONS -------------------------------------------------------------------------------*/
+		
+	/**
+	 * return errors
+	 *
+	 * @return MessageBag
+	 * @author 
+	 **/
+	public function getError()
+	{
+		return $this->errors;
+	}
+
+	/* ---------------------------------------------------------------------------- SCOPE -------------------------------------------------------------------------------*/
+
+	/* ---------------------------------------------------------------------------- QUERY BUILDER ---------------------------------------------------------------------------*/
+
+	public function scopeID($query, $variable)
+	{
+		if(is_array($variable))
+		{
+			return 	$query->whereIn('id', $variable);
+		}
+
+		return 	$query->where('id', $variable);
+	}
+
+	public function scopeDebit($query, $variable)
+	{
+		return 	$query->where('amount', '>', '0');
+	}
+
+	public  function scopeOndate($query, $variable)
+	{
+		if(!is_array($variable))
+		{
+			return $query->where('created_at', date('Y-m-d H:i:s', strtotime($variable)));
+		}
+
+		return $query->where('created_at', '>=', date('Y-m-d H:i:s', strtotime($variable[0])))->where('created_at', '<=', date('Y-m-d H:i:s', strtotime($variable[1])));
+	}
+}
