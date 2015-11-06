822033218232a23988222d3f17083d8c693a027f
chelsymooy <chelsymooy1108@gmail.com>
2015-11-06 11:51:25 +0700

    Add auditor job for transaction logs

---
+++ app/Jobs/Auditors/SaveAuditAbandonCart.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Jobs\Auditors;
+
+// change status
+use App\Jobs\Job;
+
+use App\Models\Transaction;
+use App\Models\Auditor;
+
+use App\Libraries\JSend;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use Carbon;
+
+class SaveAuditAbandonCart extends Job implements SelfHandling
+{
+    protected $transaction;
+
+    public function __construct(Transaction $transaction)
+    {
+        $this->transaction                  = $transaction;
+    }
+
+    public function handle()
+    {
+        if(is_null($this->transaction->id))
+        {
+            throw new Exception('Sent variable must be object of a record.');
+        }
+
+        $result                             = new JSend('success', (array)$this->transaction);
+
+        $previoustrans                      = Transaction::userid($this->transaction->user_id)->notid()->status('cart')->first();
+
+        if($previoustrans->count())
+        {
+            $audit                          = new Auditor;
+
+            $audit->fill([
+                    'user_id'               => $previoustrans->user_id,
+                    'ondate'                => Carbon::now()->format('Y-m-d H:i:s'),
+                    'event'                 => 'Abandoned Cart',
+                ]);
+
+            $audit->table()->associate($previoustrans);
+
+            if(!$audit->save())
+            {
+                $result                     = new JSend('error', (array)$this->transaction, $audit->getError());
+            }
+        }
+
+        return $result;
+    }
+}
