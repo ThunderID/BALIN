ead5f7b20b17a191d9f71a565e81429518348e18
uchuy77 <agil.mahendra@gmail.com>
2015-11-09 16:56:32 +0700

    modified cart & product

--- resources/views/widgets/frontend/top_menu/cart_dropdown.blade.php
+++ resources/views/widgets/frontend/top_menu/cart_dropdown.blade.php
@@ -1,29 +1,31 @@
 <?php
 	$carts = Cookie::get('baskets');
 ?>
-@if ($carts)
-	<ul class="dropdown-menu dropdown-menu-right chart-dropdown m-t-md" aria-labelledby="dLabel" style="margin-top:3px">
+<ul class="dropdown-menu dropdown-menu-right chart-dropdown m-t-md" aria-labelledby="dLabel" style="margin-top:3px">
+	@if ($carts)
 		<?php $total = 0; ?>
 		@foreach ($carts as $k => $item)
 			@if ($k==0)
 				<li class="chart-dropdown-item-grid">
-					@include('widgets.frontend.cart.cart_item', array(
+					@include('widgets.frontend.cart.cart_item', [
+						'label_id'				=> $k,
 						'label_image'			=> $item['images'],
 						'label_name'			=> $item['name'],
 						'label_qty'				=> $item['qty'],
 						'label_price'			=> $item['price'],
 						'label_total'			=> $item['qty']*$item['price']
-					))
+					])
 				</li>
 			@else
 				<li class="chart-lowLine">
-					@include('widgets.frontend.cart.cart_item', array(
+					@include('widgets.frontend.cart.cart_item', [
+						'label_id'				=> $k,
 						'label_image'			=> $item['images'],
 						'label_name'			=> $item['name'],
 						'label_qty'				=> $item['qty'],
 						'label_price'			=> $item['price'],
 						'label_total'			=> $item['qty']*$item['price']
-					))
+					])
 				</li>                        
 			@endif
 			<?php $total += ($item['price']*$item['qty']); ?>
@@ -32,17 +34,26 @@
 		<li class="chart-dropdown-subtotal chart-lowLine">
 			<div class="row">
 				<div class="col-sm-12" style="margin:5px;">
-					<p class="text-center">SUBTOTAL <Span style="font-weight:400;margin-left:25px;">@money_indo($total)</span></p>
+					<p class="text-center">SUBTOTAL <span class="m-l-md" style="font-weight:400;">@money_indo($total)</span></p>
 				</div>
 			</div>
 		</li>  
 		<li>
 			<div class="row">
 				<div class="col-xs-12 text-center" style=" ">
-					<a href="{{ URL::route('frontend.cart.index') }}" class="btn-hollow btn-hollow-sm hollow-black">Show Cart</a>
-					<a href="#" class="btn-hollow btn-hollow-sm hollow-black">Checkout</a>
+					<a href="{{ URL::route('frontend.cart.index') }}" class="btn-hollow btn-hollow-sm hollow-black m-r-sm">Lihat Cart</a>
+					<a href="#" class="btn-hollow btn-hollow-sm hollow-black m-l-sm">Checkout</a>
 				</div>
 			</div>
 		</li> 
-	</ul>
-@endif                                                                
+	@else
+		<li class="chart-dropdown-item-grid">&nbsp;</li>
+		<li class=" chart-lowLine">
+			<div class="row">
+				<div class="col-xs-12 text-center" style=" ">
+					<h4>No Cart</h4>
+				</div>
+			</div>
+		</li>
+	@endif                                                                
+</ul>
