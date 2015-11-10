ce8d72ff11d03120078726177bba826c00fb4444
budi7 <budi-purnomo@outlook.com>
2015-11-10 11:06:34 +0700

    remove product universal

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call('StoreSettingTableSeeder');
 
         $this->call('CategoryTableSeeder');
-        $this->call('ProductUniversalTableSeeder');
         $this->call('ProductTableSeeder');
         $this->call('LableTableSeeder');
 
