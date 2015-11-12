9db75ba05dfc40807554b7cdd2e700bf8a7358d9
chelsymooy <chelsymooy1108@gmail.com>
2015-11-12 09:23:20 +0700

    Add event move cookie to database when user logged

---
+++ app/Jobs/SaveToCart.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
+
+use App\Libraries\JSend;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Support\MessageBag;
+use Auth;
+
+class SaveToCart extends Job implements SelfHandling
+{
+    protected $cart;
+
+    public function __construct(array $cart)
+    {
+        $this->cart                         = $cart;
+    }
+
+    public function handle()
+    {
+        $result                             = new JSend('success', (array)$this->cart);
+
+        $errors                             = new MessageBag();
+        
+        $transaction                        = new Transaction;
+
+        $transaction->fill([
+                    'user_id'               => Auth::user()->id,
+                    'type'                  => 'sell',
+                    ]);
+
+        if($transaction->save())
+        {
+            foreach ($this->cart as $key => $value) 
+            {
+                $trsdetail                  = new TransactionDetail;
+
+                $trsdetail->fill([
+                    'transaction_id'        => $transaction['id'],
+                    'varian_id'             => $value['varian_id'],
+                    'quantity'              => $value['qty'],
+                    'price'                 => $value['price'],
+                    'discount'              => $value['discount'],
+                ]);
+
+                //cek apa punya error
+                if(!$trsdetail->save())
+                {
+                    $errors->add('Transaction', $trsdetail->getError());
+                }
+            }
+        }
+        else
+        {
+            $errors->add('Transaction', $transaction->getError());
+        }
+
+        if($errors->count())
+        {
+            $result                         = new JSend('error', (array)$this->cart, $errors);
+        }
+
+        return $result;
+    }
+}
