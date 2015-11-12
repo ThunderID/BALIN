50671ec958efb9c5e51ecae1ebab328764f1c929
budi7 <budi-purnomo@outlook.com>
2015-11-12 09:25:32 +0700

    fixed varian show

--- app/Models/Varian.php
+++ app/Models/Varian.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use DB;
 
 class Varian extends Eloquent
 {
@@ -105,6 +106,38 @@ class Varian extends Eloquent
 	}
 
 	/* ---------------------------------------------------------------------------- SCOPE -------------------------------------------------------------------------------*/
+	public function scopeGlobalStock($query, $variable)
+	{
+		return 	$query->selectraw('varians.*')
+					->selectraw('IFNULL(SUM(
+									if(transactions.type ="sell", if(transaction_logs.status ="wait" OR transaction_logs.status ="paid" OR transaction_logs.status ="shipping" OR transaction_logs.status ="delivered", 0-quantity, 0), quantity)
+									),0) as current_stock')
+					->selectraw('IFNULL(SUM(
+									if(transactions.type ="sell", if(transaction_logs.status ="wait" OR transaction_logs.status ="paid", quantity, 0), 0)
+									),0) as on_hold_stock')
+					->selectraw('IFNULL(SUM(
+									if(transactions.type ="sell", if(transaction_logs.status ="shipping" OR transaction_logs.status ="delivered", 0-quantity, 0), quantity)
+									),0) as inventory_stock')
+					->selectraw('IFNULL(SUM(
+									if(transactions.type ="sell", if(transaction_logs.status ="paid", quantity, 0), 0)
+									),0) as reserved_stock')
+					->selectraw('IFNULL(SUM(
+									if(transactions.type ="sell", if(transaction_logs.status ="wait" OR transaction_logs.status ="paid", 0-quantity, 0), quantity)
+									),0) as bought_stock')
+					->join('transaction_details', 'transaction_details.varian_id', '=', 'varians.id')
+					->join('transactions', 'transactions.id', '=', 'transaction_details.transaction_id')
+					->join(DB::raw('(SELECT status, transaction_id, changed_at from transaction_logs as tlogs1 where changed_at = (SELECT MAX(changed_at) FROM transaction_logs AS tlogs2 WHERE tlogs1.transaction_id = tlogs2.transaction_id and tlogs2.deleted_at is null) and tlogs1.deleted_at is null group by transaction_id) as transaction_logs'), function ($join) use($variable) 
+						{
+							$join
+								->on('transaction_logs.transaction_id', '=', 'transactions.id')
+								->whereIn('transaction_logs.status' , ['wait', 'paid', 'shipping', 'delivered'])
+								;
+						})
+					->whereIn('transactions.type', ['sell', 'buy'])
+					->groupby('varians.id')
+					;
+		;
+	}
 
 	/* ---------------------------------------------------------------------------- QUERY BUILDER ---------------------------------------------------------------------------*/
 
