d98a53f795238804e0c619f26fe3ab5a9b6ea3d3
chelsymooy <chelsymooy1108@gmail.com>
2015-11-14 12:45:23 +0700

    another bug on transaction restored

--- app/Jobs/Models/Transaction/TransactionRestored.php
+++ app/Jobs/Models/Transaction/TransactionRestored.php
@@ -6,11 +6,14 @@ use App\Jobs\Job;
 use App\Libraries\JSend;
 
 use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 
 use App\Models\Transaction;
 
 class TransactionRestored extends Job implements SelfHandling
 {
+    use DispatchesJobs;
+    
     protected $transaction;
 
     public function __construct(Transaction $transaction)
