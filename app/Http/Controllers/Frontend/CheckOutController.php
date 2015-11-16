8a3e1bb2b737279e6463d0f4dd865eb38005dbf0
chelsymooy <chelsymooy1108@gmail.com>
2015-11-16 08:27:45 +0700

    finish shipment and checkout

--- app/Http/Controllers/Frontend/CheckOutController.php
+++ app/Http/Controllers/Frontend/CheckOutController.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\BaseController;
 use App\Models\Transaction;
 use App\Models\Address;
+use App\Models\Voucher;
+use App\Models\Shipment;
+use App\Models\Courier;
 use Input, Response, Redirect, Cookie, Auth;
 
 class CheckOutController extends BaseController 
@@ -28,6 +31,71 @@ class CheckOutController extends BaseController
 
 	public function postCheckout()
 	{
-		return $this->layout;
+		$transaction           	 				= Transaction::userid(Auth::user()->id)->status('cart')->wherehas('transactiondetails', function($q){$q;})->with(['transactiondetails', 'transactiondetails.varian', 'transactiondetails.varian.product'])->first();
+		
+		if(!$transaction)
+		{
+			return Redirect::back()->withErrors('Tidak ada keranjang.');
+		}
+
+		if(Input::has('voucher_code') && Input::Get('voucher_code')!='')
+		{
+			$vouchers 							= Voucher::code(Input::get('voucher_code'))->ondate('now')->first();
+
+			if(!$vouchers)
+			{
+				return Redirect::back()->withErrors('Tidak ada voucher.');
+			}
+
+			$transaction->fill(['voucher_id' => $vouchers->id]);
+
+			if(!$transaction->save())
+			{
+				return Redirect::back()->withErrors($transaction->getError());
+			}
+		}
+
+		if(Input::has('address_id') && Input::Get('address_id')!=0)
+		{
+			$address 							= Address::findorfail(Input::get('address_id'));
+		}
+		else
+		{
+			$input 								= Input::only('address', 'zipcode', 'phone', 'receiver_name');
+			$address 							= new Address;
+			$address->fill($input);
+
+			if(!$address->save())
+			{
+				return Redirect::back()->withErrors($address->getError());
+			}
+		}
+
+		$courier 								= Courier::first();
+		if(!$courier)
+		{
+			return Redirect::back()->withErrors('Tidak ada kurir.');
+		}
+
+		$shipment 								= new Shipment;
+
+		$shipment->fill([
+				'courier_id'					=> $courier->id,
+				'transaction_id'				=> $transaction->id,
+				'address_id'					=> $address->id,
+				'receiver_name'					=> $address->receiver_name,
+		]);
+
+		//bisa jadi setelah term and condition
+		$result                         		= $this->dispatch(new ChangeStatus($transaction, 'wait'));
+
+		if($result->getStatus()=='success')
+		{
+			Cookie::forget('baskets');
+
+			return Redirect::route('frontend.profile.order.show', $transaction->ref_number);
+		}
+
+		return Redirect::back()->withErrors($result->getErrorMessage());
 	}
 }
