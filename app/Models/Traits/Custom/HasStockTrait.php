fabc6dabd58d771fdbcca5ecfd2c61caa99058b6
chelsymooy <chelsymooy1108@gmail.com>
2015-11-20 16:08:00 +0700

    Ux certain moment

--- app/Models/Traits/Custom/HasStockTrait.php
+++ app/Models/Traits/Custom/HasStockTrait.php
@@ -15,6 +15,14 @@ trait HasStockTrait
 		//
 	}
 
+	public function scopeSelectStockInCart($query, $variable)
+	{
+		return $query->selectraw('IFNULL(SUM(
+									if(transactions.type ="sell", if(transaction_logs.status ="cart", quantity, 0), 0)
+									),0) as cart_item')
+		;
+	}
+	
 	public function scopeSelectCurrentStock($query, $variable)
 	{
 		return $query->selectraw('IFNULL(SUM(
