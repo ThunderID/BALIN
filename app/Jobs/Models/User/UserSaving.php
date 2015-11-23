2049c9169dc3de75a9a4cb037091c0e9ee43b454
chelsymooy <chelsymooy1108@gmail.com>
2015-11-23 10:05:10 +0700

    repair unique email

--- app/Jobs/Models/User/UserSaving.php
+++ app/Jobs/Models/User/UserSaving.php
@@ -26,6 +26,23 @@ class UserSaving extends Job implements SelfHandling
 	public function handle()
 	{
 		$result								= new JSend('success', (array)$this->user);
+		
+		if(is_null($this->user->id))
+		{
+			$id 							= 0;
+		}
+		else
+		{
+			$id 							= $this->user->id;
+		}
+
+		$user 								= User::email($this->user->email)->notid($id)->first();
+
+		if($user->count())
+		{
+			$result							= new JSend('error', (array)$this->user, 'Email sudah terdaftar.');
+		}
+
 
 		if (Hash::needsRehash($this->user->password))
 		{
