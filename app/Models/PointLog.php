8f28a4f061f44b0724fdac5c1a4195ef256556ca
chelsymooy <chelsymooy1108@gmail.com>
2015-11-26 18:28:58 +0700

    using abs, change point color, group by reference

--- app/Models/PointLog.php
+++ app/Models/PointLog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use DB;
 
 class PointLog extends Eloquent
 {
@@ -150,5 +151,16 @@ class PointLog extends Eloquent
 		}
 
 		return 	$query;
+	}
+
+	public function scopeCurrentPoint($query, $variable)
+	{
+		return 	$query->selectraw('point_logs.*')
+		->selectraw('IFNULL(SUM(point_logs.amount),0) as amount')
+		->selectraw('IFNULL(SUM(point_logs.amount),0) as prev_amount')
+		->selectraw('(SELECT IFNULL(SUM(plogs.amount),0) FROM point_logs as plogs join point_logs on plogs.user_id = point_logs.user_id WHERE plogs.user_id = '.$variable.' and plogs.user_id = '.$variable.' and plogs.created_at < point_logs.created_at and plogs.deleted_at is null and point_logs.deleted_at is null and plogs.expired_at >= "'.date('Y-m-d H:i:s').'" and point_logs.expired_at >= "'.date('Y-m-d H:i:s').'") as prev_amount')
+		->where('point_logs.expired_at', '>=', date('Y-m-d H:i:s'))
+		->userid($variable)->groupby(['point_logs.reference_type', 'point_logs.reference_id'])
+		;
 	}
 }
