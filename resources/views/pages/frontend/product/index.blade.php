c3ab4b040630af310fb273d743abfffd7e459be9
budi7 <budi-purnomo@outlook.com>
2015-11-26 14:42:21 +0700

    fixed product index pilih category

--- resources/views/pages/frontend/product/index.blade.php
+++ resources/views/pages/frontend/product/index.blade.php
@@ -211,7 +211,7 @@
 			    	<div class="modal-content">
 						<div class="modal-header">
 				        	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				       		<h4 class="modal-title" id="exampleModalLabel">Kategori</h4>
+				       		<h4 class="modal-title" id="exampleModalLabel">Pilih Kategori</h4>
 				      	</div>
 				      	<div class="modal-body ribbon-menu">
 							<ul class="list-inline m-b-none">
