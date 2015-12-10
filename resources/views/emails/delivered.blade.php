dec475c65e7de64101716b67ff095b7ff4e18752
Agil Mahendra <agil.mahendra@gmail.com>
2015-12-10 10:27:19 +0700

    email & page checkout
    - change caption typo delivered in email
    - change caption in checkout
    - add tabel rincian pesananan

--- resources/views/emails/delivered.blade.php
+++ resources/views/emails/delivered.blade.php
@@ -40,7 +40,7 @@
 			<td style="width:3%; text-align:center; padding: 5px; background-color: #000; color: #fff;">
 				<div style="width:50px; margin: 0 auto;">
 					<div>4</div>
-					<p style="margin-bottom:0;">Deliverd</p>
+					<p style="margin-bottom:0;">Delivered</p>
 				</div>
 			</td>
 			<td class="col-sm-1" style="width:10%">&nbsp;</td>
@@ -57,6 +57,45 @@
 							<p> 
 								Menurut pantauan kami pesanan dengan nomor invoice #{{$data['delivered']['ref_number']}} dengan nomor resi #{{$data['delivered']['shipment']['receipt_number']}} sudah sampai dialamat penerima dan {{$data['notes']}}.
 							</p>
+						</td>
+						<td>
+							<h3>Rincian Pesanan</h3>
+							<table style="width:100%; font-size:11px;">
+								<thead>
+									<tr>
+										<th class="col-md-1 text-center" style="text-align:center;background-color:black;color:white;padding:10px;">No</th>
+										<!-- <th>Item#</th> -->
+										<th class="text-center col-md-4" style="text-align:center;background-color:black;color:white;padding:10px;">Item</th>
+										<th class="text-center col-md-1" style="text-align:center;background-color:black;color:white;padding:10px;">Qty</th>
+										<th class="text-right col-md-2" style="text-align:center;background-color:black;color:white;padding:10px;">Harga @</th>
+										<th class="text-right col-md-2" style="text-align:center;background-color:black;color:white;padding:10px;">Diskon</th>
+										<th class="text-right col-md-2" style="text-align:center;background-color:black;color:white;padding:10px;">Total</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php $amount = 0;?>
+									@forelse($data['delivered']['transactiondetails'] as $key => $value)
+										<?php $amount = $amount + (($value['price'] - $value['discount']) * $value['quantity']);?>
+										<tr>
+											<td class="text-center" style="text-align:center;background-color:#C6C6C6;padding:5px;">{!!($key+1)!!}</td>
+											<td style="text-align:left;background-color:#C6C6C6;padding:5px;"> {{$value['varian']['product']['name']}} {{$value['varian']['size']}}</td>
+											<td class="text-center" style="text-align:center;background-color:#C6C6C6;padding:5px;"> {{$value['quantity']}} </td>
+											<td class="text-right" style="text-align:right;background-color:#C6C6C6;padding:5px;"> @money_indo($value['price']) </td>
+											<td class="text-right" style="text-align:right;background-color:#C6C6C6;padding:5px;"> @money_indo($value['discount']) </td>
+											<td class="text-right" style="text-align:right;background-color:#C6C6C6;padding:5px;"> @money_indo((($value['price'] - $value['discount']) * $value['quantity'])) </td>
+										</tr>
+									@empty
+										<tr>
+											<td colspan="6"> Tidak ada data </td>
+										</tr>
+									@endforelse
+									<tr>
+										<td colspan="6">&nbsp;</td>
+									</tr>
+								</tbody>
+							</table>
+						</td>
+						<td>
 							<p>
 								Terima kasih untuk kepercayaan anda. 
 							</p>
